05157ad741b39e8eb8178d0fbc2f4129746d718a
David PAGNACCO <david.pagnacco@gmail.com>
2026-01-10 10:24:31 +0100

    fix(db): drop duplicate index media_idx before creation in migration 0003

--- libs/backend/infrastructure/drizzle/0003_lazy_logan.sql
+++ libs/backend/infrastructure/drizzle/0003_lazy_logan.sql
@@ -8,4 +8,5 @@ CREATE TABLE "interaction"."user_follows" (
 CREATE UNIQUE INDEX "user_follows_pk" ON "interaction"."user_follows" USING btree ("follower_id","following_id");--> statement-breakpoint
 CREATE INDEX "follower_idx" ON "interaction"."user_follows" USING btree ("follower_id");--> statement-breakpoint
 CREATE INDEX "following_idx" ON "interaction"."user_follows" USING btree ("following_id");--> statement-breakpoint
+DROP INDEX IF EXISTS "interaction"."media_idx";--> statement-breakpoint
 CREATE INDEX "media_idx" ON "interaction"."user_interactions" USING btree ("media_id");
