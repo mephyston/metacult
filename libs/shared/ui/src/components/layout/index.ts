5e986daae3d471dd41b712fcb0ebde26dc0b870c
David PAGNACCO <david.pagnacco@gmail.com>
2026-02-01 22:29:13 +0100

    fix(ui): export Search component from layout barrel

--- libs/shared/ui/src/components/layout/index.ts
+++ libs/shared/ui/src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header.vue';
 export { default as Footer } from './Footer.vue';
 export { default as TabBar } from './TabBar.vue';
+export { default as Search } from './Search.vue';
