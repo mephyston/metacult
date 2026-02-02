c5555a82c130786f7422294466783ddf15ed9dae
David PAGNACCO <david.pagnacco@gmail.com>
2026-02-02 22:40:17 +0100

    fix(ide): resolve unused variable and duplicated code warnings

--- libs/shared/ui/src/components/ui/slider/Slider.vue
+++ libs/shared/ui/src/components/ui/slider/Slider.vue
@@ -51,7 +51,7 @@ const value = computed({
     </SliderTrack>
     <SliderThumb
       v-for="(val, index) in value"
-      :key="index"
+      :key="`${index}-${val}`"
       class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
     />
   </SliderRoot>
